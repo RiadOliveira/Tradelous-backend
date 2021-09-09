c9e87d98e4168950d2c40bdbbd1da745a6efed39
RiadOliveira <riad.oliveira@hotmail.com>
2021-09-09 19:04:14 -0300

    CORS added

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import '../container';
 
+import cors from 'cors';
 import GlobalErrorHandler from '../errors/GlobalErrorHandler';
 import routes from './routes';
 import path from 'path';
@@ -12,6 +13,11 @@ import path from 'path';
 const app = express();
 
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    }),
+);
 app.use('/files', express.static(path.resolve('tmp', 'uploads')));
 app.use(routes);
 app.use(GlobalErrorHandler);
