d04f415623a6ced38bda23b41b3036c46b39bf7f
RiadOliveira <riad.oliveira@hotmail.com>
2022-01-04 09:53:11 -0300

    Company's cascade delete when admin deleted

--- src/shared/typeorm/migrations/1611030505592-CreateForeignAdminIdInCompanies.ts
+++ src/shared/typeorm/migrations/1611030505592-CreateForeignAdminIdInCompanies.ts
@@ -10,7 +10,7 @@ export class CreateForeignAdminIdInCompanies1611030505592
                 referencedColumnNames: ['id'],
                 referencedTableName: 'users',
                 onUpdate: 'RESTRICT',
-                onDelete: 'SET NULL',
+                onDelete: 'CASCADE',
             }),
         );
     }
